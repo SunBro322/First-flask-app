d5bdebb610fd1f2d6471d6a5ad003a7ce0ee9060
SunBro <andrei_chesanov@mail.ru>
2025-03-04 01:59:46 +0300

    Search and Filter users, add new form

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify, render_template
+
+
+from flask import Flask, jsonify, render_template, request
 
 # Это callable WSGI-приложение
 app = Flask(__name__)
@@ -36,10 +38,24 @@ def courses_show(id):
 def not_found(error):
     return 'Page not found', 404
 
-@app.route('/users/<id>')
-def show_user(id):
-    """ Отображение пользователя с выводом HTML"""
-    return render_template(
-        'show.html',
-        id=id,
-    )
+# @app.route('/users/<id>')
+# def show_user(id):
+#     """ Отображение пользователя с выводом HTML"""
+#     return render_template(
+#         'show.html',
+#         id=id,
+#     )
+
+users = [
+    {'id': 1, 'name': 'mike'},
+    {'id': 2, 'name': 'mishel'},
+    {'id': 3, 'name': 'adel'},
+    {'id': 4, 'name': 'keks'},
+    {'id': 5, 'name': 'kamila'}
+]
+
+@app.route('/users')
+def show_users():
+    search_term = request.args.get('search', '').lower()
+    filtered_users = [user for user in users if search_term in user['name'].lower()]
+    return render_template('users/index.html', users=filtered_users)
