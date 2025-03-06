4ea1a8de58e6fe3bace4b97e78ead7dd3f739f39
SunBro <andrei_chesanov@mail.ru>
2025-03-06 19:34:28 +0300

    Add new users, filter users, add function with 500 code

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,16 @@
+import json
+import os.path
+from contextlib import redirect_stderr
+from typing import List
 
-
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 
 # Это callable WSGI-приложение
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello!'
+    return redirect(url_for('get_users'))
 
 # @app.get('/users')
 # def users_get():
@@ -38,6 +41,10 @@ def courses_show(id):
 def not_found(error):
     return 'Page not found', 404
 
+@app.errorhandler(500)
+def arise_errors(error):
+    return 'Script has errors', 500
+
 # @app.route('/users/<id>')
 # def show_user(id):
 #     """ Отображение пользователя с выводом HTML"""
@@ -46,16 +53,58 @@ def not_found(error):
 #         id=id,
 #     )
 
-users = [
-    {'id': 1, 'name': 'mike'},
-    {'id': 2, 'name': 'mishel'},
-    {'id': 3, 'name': 'adel'},
-    {'id': 4, 'name': 'keks'},
-    {'id': 5, 'name': 'kamila'}
-]
+# users = [
+#     {'id': 1, 'name': 'mike'},
+#     {'id': 2, 'name': 'mishel'},
+#     {'id': 3, 'name': 'adel'},
+#     {'id': 4, 'name': 'keks'},
+#     {'id': 5, 'name': 'kamila'}
+# ]
 
-@app.route('/users')
-def show_users():
+@app.route('/find_user')
+def find_user():
+    """ Поиск пользователей """
     search_term = request.args.get('search', '').lower()
+    users = load_users()
     filtered_users = [user for user in users if search_term in user['name'].lower()]
     return render_template('users/index.html', users=filtered_users)
+
+def load_users() -> List:
+    """ Считывание данных из Json """
+    if not os.path.exists(r"..\First-flask-app\data base\user.json"):
+        return []
+
+    with open(r"..\First-flask-app\data base\user.json") as f:
+        try:
+            users = json.load(f)
+        except json.JSONDecodeError:
+            users = []
+    return users
+
+def save_user(users):
+    """ Сохранение данных по пользователям """
+    with open(r'..\First-flask-app\data base\user.json', 'w') as f:
+        json.dump(users, f, indent=4)
+
+@app.route('/create-users', methods=['POST', 'GET'])
+def create_user():
+    """ Создание нового пользователя """
+    if request.method:
+        name = request.form.get('name')
+        email = request.form.get('email')
+        if name and email:
+            users = load_users()
+            new_id = max(user['id'] for user in users) + 1 if users else 1
+            new_user = {'id': new_id,
+                        'name': name,
+                        'email': email}
+            users.append(new_user)
+            save_user(users)
+            return redirect(url_for('get_users'))
+    return render_template('users/create_user.html')
+
+@app.route('/users')
+def get_users():
+    """ Вывод пользователей """
+    users = load_users()
+    return render_template('users/users.html', users=users)
